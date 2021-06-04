66f686b3eb96c186a2a35f46a348f7380c478386
ikalpesh <kalpeshmahajan90@gmail.com>
2021-06-04 16:17:48 +0530

    Percentage Of singlet Combination

--- FlipCoinSimulator-2.sh
+++ FlipCoinSimulator-2.sh
@@ -1,39 +1,22 @@
-echo "Flip Coin Combination"
-
+heads=1
+tails=0
 declare -A res
+
 echo "Enter n for the loop "
 read n
-hh=0
-ht=1
-th=2
-tt=3
 for (( i=0;i<n;i++ ))
 do
-   toss=$((RANDOM%4))
-   if [ $toss -eq $hh ]
+   toss=$((RANDOM%2))
+   if [ $toss -eq $heads ]
    then
-       res['hh']=$(("${res['hh']}"+1))
-   elif [ $toss -eq $ht ]
-   then
-       res['ht']=$(("${res['ht']}"+1))
-   elif [ $toss -eq $th ]
-   then
-       res['th']=$(("${res['th']}"+1))
-
+       res['heads']=$(("${res['heads']}"+1))
    else
-       res['tt']=$(("${res['tt']}"+1))
+       res['tails']=$(("${res['tails']}"+1))
 
    fi
 done
-hhcnt="${res['hh']}"
-htcnt="${res['ht']}"
-ttcnt="${res['tt']}"
+headcnt=$(($n-${res['heads']}))
+headpercent=$((($headcnt*100)/$n))
+echo "tails=${res['tails']} and heads=${res['heads']}"
+echo "occurance of tails = $headpercent% and head = $((100-$headpercent))%" 
 
-hhpercent=$((($hhcnt*100)/$n))
-htpercent=$((($htcnt*100)/$n))
-ttpercent=$((($ttcnt*100)/$n))
-thpercent=$((100-$hhpercent-$htpercent-$ttpercent))
-headpercent=$((100-$ttpercent-($thpercent+$htpercent)/2))
-echo "hh=${res['hh']}, ht=${res['ht']}, th=${res['th']} and tt=${res['tt']}"
-echo "hh=$hhpercent%, ht=$htpercent%, th=$thpercent% and tt=$ttpercent%"
-echo "heads=$headpercent% and tails=$((100-$headpercent))%"
